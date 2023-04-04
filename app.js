8261b6b23a112eba4fef1010d7a152002359a9e1
saidi2307 <saidireddy0546@gmail.com>
2023-04-04 17:40:39 -0500

    bonus

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ballRouter = require('./routes/ball');
+var boardRouter = require('./routes/board');
+var selectRouter=require('./routes/selector')
 
 var app = express();
 
@@ -23,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ball', ballRouter);
+app.use('/board',boardRouter);
+app.use('/selector',selectRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
